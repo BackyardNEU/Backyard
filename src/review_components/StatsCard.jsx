ef9d6a52687c8de8eb71620f4cdd4a63b3c91b2d
MiloB07 <bell.mil@northeastern.edu>
2026-03-26 23:36:08 -0400

    Fixed no stats crash

--- src/review_components/StatsCard.jsx
+++ src/review_components/StatsCard.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import "./ReviewList.css"
 
 export default function StatsCard({ stats_array }) {
-
+     if (!stats_array) {
+        return <div className="stats-card"><p>No stats available.</p></div>;
+    }
     console.log(stats_array);
 
     const stat_categories = [
-        { label: "Time commitment", value: parseFloat(stats_array?.hours.toFixed(1)) || 0, color: 'rgba(82, 50, 6, 1)', end: 12 },
-        { label: "Skill Growth Index", value: parseFloat(stats_array?.skill.toFixed(1)) || 0, color: 'rgb(47, 115, 164)', end: 10 },
-        { label: "Community", value: parseFloat(stats_array?.community.toFixed(1)) || 0, color: 'rgba(255, 128, 0, 1)', end: 10 },
-        { label: "Leadership", value: parseFloat(stats_array?.leadership.toFixed(1)) || 0, color: 'rgba(198, 165, 1, 0.85)', end: 10 },
-        { label: "Fun Index", value: parseFloat(stats_array?.fun_index.toFixed(1)) || 0, color: 'rgba(124, 124, 124, 0.85)', end: 10 },
+        { label: "Time commitment", value: parseFloat(stats_array.hours?.toFixed(1)) || 0, color: 'rgba(82, 50, 6, 1)', end: 12 },
+        { label: "Skill Growth Index", value: parseFloat(stats_array.skill?.toFixed(1)) || 0, color: 'rgb(47, 115, 164)', end: 10 },
+        { label: "Community", value: parseFloat(stats_array.community?.toFixed(1)) || 0, color: 'rgba(255, 128, 0, 1)', end: 10 },
+        { label: "Leadership", value: parseFloat(stats_array.leadership?.toFixed(1)) || 0, color: 'rgba(198, 165, 1, 0.85)', end: 10 },
+        { label: "Fun Index", value: parseFloat(stats_array.fun_index?.toFixed(1)) || 0, color: 'rgba(124, 124, 124, 0.85)', end: 10 },
     ];
 
     return (
