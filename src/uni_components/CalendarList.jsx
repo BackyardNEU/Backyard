8120970ed1b1aaa16dbb8d7df19a0daf49566eaa
Ryan Marshall <ryawmar@gmail.com>
2026-04-02 20:12:45 -0400

    made some more progess with adding events should set up future infrastucture pretty well

--- src/uni_components/CalendarList.jsx
+++ src/uni_components/CalendarList.jsx
@@ -3,12 +3,11 @@ import {  } from 'date-fns';
 
 //events will be the array of club events happening that match a user's favorited clubs. If empty, no events will be displayed and a special message will appear.
 export const CalendarList = ({ events }) => {
-
-    
+    const message = "HELLO!!!!1"
     // use the date-fns component here
     return (
         <div>
-            
+            {message}
         </div>
     );
 };
